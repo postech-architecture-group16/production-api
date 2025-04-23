574137c8392a2ee4cb672c337f3a1bd59be97d68
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-04-23 13:47:47 -0300

    Initial commit

---
+++ src/main/java/com/fiap/challenge/production/ProductionApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionApiApplication.class, args);
+	}
+
+}
