baea005c1c56c6a6c786b313ed6678d6c72f4d39
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-05-10 02:46:32 -0300

    Changing rabbitMq for SQS

--- src/main/java/com/fiap/challenge/production/ProductionApiApplication.java
+++ src/main/java/com/fiap/challenge/production/ProductionApiApplication.java
@@ -1,10 +1,8 @@
 package com.fiap.challenge.production;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableRabbit
 @SpringBootApplication
 public class ProductionApiApplication {
 
